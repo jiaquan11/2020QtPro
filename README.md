9840e25852dc0e359f282793fe0117f5352ef03f
jiaquan11 <913882743@qq.com>
2021-03-13 10:09:28 +0800

    *test_rgb_qt项目: 在VS2019中使用QT窗口显示一个rgb图像

---
+++ README.md
@@ -0,0 +1,12 @@
+# 2020QtPro
+这是一个2020年编写的一些QT相关的项目集合仓库
+
+xiacaojun Teacher
+1.QVS项目
+在VS2015中集成Qt的插件，创建的一个Qt的测试环境项目
+
+2.first_qt项目
+在VS2019中集成Qt的插件，创建的一个Qt的测试环境项目
+
+3.test_rgb_qt项目
+在VS2019中使用QT窗口显示一个rgb图像
---
+++ XiaCaoJun_Teacher/src/test_rgb_qt/testrgb.cpp
@@ -0,0 +1,37 @@
+#include "testrgb.h"
+#include <QPainter>
+#include <QImage>
+
+static int w = 1280;
+static int h = 720;
+
+TestRGB::TestRGB(QWidget *parent)
+    : QWidget(parent)
+{
+    ui.setupUi(this);
+
+    resize(w, h);//´°¿Ú´óÐ¡
+}
+
+void TestRGB::paintEvent(QPaintEvent* ev) {
+    QImage img(w, h, QImage::Format_RGB888);
+    auto d = img.bits();
+    unsigned char r = 255;
+    for (int j = 0; j < h; j++) {
+        r--;
+
+        int b = j * w * 3;
+        for (int i = 0; i < w * 3; i+=3){
+            d[b + i] = r;//R
+            d[b + i + 1] = 0;//G
+            d[b + i + 2] = 0;//B
+        }
+    }
+
+    QPainter p;
+    p.begin(this);
+
+    p.drawImage(0, 0, img);
+
+    p.end();
+}
